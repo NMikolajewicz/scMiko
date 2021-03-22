ef88b39afd17971df3eac4695fa4bbdc1be555a6
Nicholas Mikolajewicz <n.mikolajewicz@utoronto.ca>
2021-03-22 17:59:25 -0400

    misc. updates

--- R/analysis_functions.R
+++ R/analysis_functions.R
@@ -262,3 +262,131 @@ runAUC <- function(object, genelist, assay = DefaultAssay(object), n.workers = 1
 
 }
 
+
+#' Run Modular Scoring
+#'
+#' Wrapper for Seurat::AddModuleScore. Calculates module scores for feature expression programs in single cells.
+#'
+#' @param object Seurat object
+#' @param genelist Named list of genesets.
+#' @param assay Assay used for expression matrix.
+#' @param score.key Expression program prefix. default is "MS".
+#' @param ... additional parameters passed to geom_point(...)
+#' @name runMS
+#' @seealso \code{\link{AddModuleScore}}
+#' @author Nicholas Mikolajewicz
+#' @return list of results along with ggplot handles visualizing class predictions overlaid on UMAP.
+#' @examples
+#'
+#' # get genesets
+#' verhaak.df <- geneSets[["Verhaak_CancerCell_2010"]]
+#' verhaak.list <- wideDF2namedList(verhaak.df)
+#'
+#' gsc.df <- geneSets[["Richards_NatureCancer_2021_sc"]]
+#' gsc.list <- wideDF2namedList(gsc.df)
+#'
+#' neftel.df <- geneSets[["GBM_Hs_Neftel2019"]]
+#' neftel.list <- wideDF2namedList(neftel.df)
+#'
+#' verhaak.list <- lapply(verhaak.list, toupper)
+#' gsc.list <- lapply(gsc.list, toupper)
+#' neftel.list <- lapply(neftel.list, toupper)
+#'
+#' # classify cells based on provided genesets
+#' v.auc <- runMS(object = so.query, genelist = verhaak.list)
+#' v.auc$plot.max.score
+#'
+#' g.auc <- runMS(object = so.query, genelist = gsc.list)
+#' g.auc$plot.max.score
+#'
+#' n.auc <- runMS(object = so.query, genelist = neftel.list)
+#' n.auc$plot.max.score
+#'
+runMS <- function(object, genelist, assay = DefaultAssay(object), score.key = "MS", ...){
+
+  message("Scoring gene modules...")
+  object <-   Seurat::AddModuleScore(
+    object = object,
+    features = genelist,
+    pool = NULL,
+    nbin = 24,
+    ctrl = 100,
+    k = FALSE,
+    assay = NULL,
+    name = score.key,
+    seed = 1,
+    search = FALSE
+  )
+
+
+  df.ms <- object@meta.data[ ,grepl(score.key, colnames(object@meta.data) )]
+  colnames(df.ms) <- names(genelist)
+
+  message("Consolidating results...")
+  which.max.score <- apply(df.ms, 1, which.max)
+  class.prediction.max <- colnames(df.ms)[which.max.score]
+
+  df.auc.umap <- data.frame(
+    cells = colnames(object),
+    x = object@reductions[["umap"]]@cell.embeddings[ ,1],
+    y = object@reductions[["umap"]]@cell.embeddings[ ,2]
+  )
+
+  df.auc.umap <- bind_cols(df.auc.umap, df.ms)
+  df.auc.umap$class.ms <- class.prediction.max
+
+  if ((length(genelist) > 10)){
+    colpal <- scales::hue_pal()(length(genelist))
+  } else if ((length(genelist) <= 10) & (length(genelist) > 1)){
+    colpal <- ggthemes::ptol_pal()(length(genelist))
+  } else {
+    colpal <- "tomato"
+  }
+  colpal <- c(colpal, "grey")
+  names(colpal) <- c(names(genelist), "unresolved")
+
+  df.auc.umap$class.ms <- factor(df.auc.umap$class.ms, levels = names(colpal))
+  message("Generating plots...")
+  plt.umap.list <- list()
+  for (i in 1:ncol(df.ms)){
+
+    which.set <- colnames(df.ms)[i]
+    df.auc.umap2 <- df.auc.umap
+    df.auc.umap2$auc <- df.auc.umap2[ ,which.set]
+
+    plt.umap.list[[which.set]] <- df.auc.umap2 %>%
+      ggplot(aes(x = x, y = y, color = auc)) +
+      geom_point(...) +
+      labs(x = "UMAP 1", y = "UMAP 2", color = "Score",  title = which.set, subtitle = "Modular scores") +
+      theme_miko(center.title = T, legend = T) +
+      scale_color_gradient2(low = muted("blue"),
+                            mid = "white",
+                            high = muted("red"))
+  }
+
+  plt.max.umap <- df.auc.umap %>%
+    dplyr::arrange(-as.numeric(class.ms)) %>%
+    ggplot(aes(x = x, y = y, color = class.ms)) +
+    # geom_point() +
+    geom_point(...) +
+    labs(x = "UMAP 1", y = "UMAP 2", caption = "Classification based on max modular score", color = "Class") +
+    theme_miko(center.title = T, legend = T) +
+    scale_color_manual(values = colpal) +
+    guides(fill = F, color = guide_legend(override.aes = list(size = 4)))
+
+
+
+  closeAllConnections()
+  rm(object)
+  invisible({gc()})
+
+  return(
+    list(
+      data = df.auc.umap,
+      plot.max.score = plt.max.umap, # max score across genesets (-rejection)
+      plot.list = plt.umap.list,
+      class.prediction = class.prediction.max
+    )
+  )
+
+}
