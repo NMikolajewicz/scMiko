7245646a3bfd440f3855753dbe87cf9e5a387c01
Nicholas Mikolajewicz <n.mikolajewicz@utoronto.ca>
2022-03-18 15:54:24 -0400

    added Transcriptional hallmarks of intra-tumor heterogeneity (Gavish 2022) genesets

--- R/datasets.R
+++ R/datasets.R
@@ -43,6 +43,8 @@
 #' \item{CancerSCEM (\href{https://ngdc.cncb.ac.cn/cancerscem/documents/#cellTypeDisL}{link})}{Cell-type specific marker genes used by CancerSCEM.}
 #' \item{Stem_Catalog}{Stemness markers, literature curated. Sources include (\href{http://193.136.227.155/stemchecker/stemsethome.jsf}{StemChecker}, \href{https://www.sciencedirect.com/science/article/pii/S0092867418303581#mmc1}{Malta}, \href{https://bmccancer.biomedcentral.com/articles/10.1186/s12885-021-08351-0}{Hong}, \href{https://www.nature.com/articles/nature07056}{Mikkelsen}, \href{https://link.springer.com/article/10.1186/gb-2012-13-8-r71#ref-CR32}{Palmer}, \href{https://ashpublications.org/blood/article/103/8/2956/18022/Gene-expression-in-human-embryonic-stem-cell-lines}{Bhattacharya}, \href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6313194/}{Dong}, \href{https://www.thelancet.com/journals/ebiom/article/PIIS2352-3964(19)30114-8/fulltext#supplementaryMaterial}{Pece}, \href{https://translational-medicine.biomedcentral.com/articles/10.1186/s12967-020-02527-1}{Feng}, \href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0215452#pone.0215452.s002}{Schulten})}
 #' \item{HALLMARK (\href{https://www.cell.com/fulltext/S2405-4712(15)00218-5}{link})}{50 “hallmark” gene sets from the Molecular Signature Database (MSigDB)}
+#' \item{Gavish2022_Tumor_Hallmarks (\href{https://www.biorxiv.org/content/10.1101/2021.12.19.473368v1}{link})}{Transcriptional hallmarks of intra-tumor heterogeneity}
+
 #' }
 #'
 #' @keywords genesets, cell annotation, module scoring
